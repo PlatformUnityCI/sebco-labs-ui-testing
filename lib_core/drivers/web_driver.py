b1f5bd760d7ef59a9502987ed4df0af31f4fa933
Sebastián Couto <sebastian.o.couto@gmail.com>
2026-04-30 13:47:04 -0300

    adjustmen on web driver init

--- lib_core/drivers/web_driver.py
+++ lib_core/drivers/web_driver.py
@@ -1,10 +1,24 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+import os
+
 
 def get_web_driver():
     options = webdriver.ChromeOptions()
-    options.add_argument("--start-maximized")
 
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+    # 👇 siempre útil
+    options.add_argument("--window-size=1920,1080")
+
+    # 👇 SOLO en CI
+    if os.getenv("CI"):
+        options.add_argument("--headless=new")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
+
+    driver = webdriver.Chrome(
+        service=Service(ChromeDriverManager().install()),
+        options=options
+    )
+
     return driver
